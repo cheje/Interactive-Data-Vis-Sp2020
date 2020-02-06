b49a9a6cd61825fb425aea7c26ecf9b959eea521
cheje <jcheng1@gradcenter.cuny.edu>
2020-02-06 02:26:53 -0500

    Change data and update code

--- tutorial1_intro/main.js
+++ tutorial1_intro/main.js
@@ -1,5 +1,5 @@
 // load in csv
-d3.csv("https://raw.githubusercontent.com/cheje/Interactive-Data-Vis-Sp2020/master/tutorial1_intro/data/nyc_jan_temps.csv").then(data => {
+d3.csv("data/commuting_states.csv").then(data => {
   console.log("data", data);
 
   // select single instance of table
@@ -11,8 +11,9 @@ d3.csv("https://raw.githubusercontent.com/cheje/Interactive-Data-Vis-Sp2020/mast
     .append("tr")
     .append("th")
     .attr("colspan", "12")
-    .text("High Temperatures in NYC Over the Past Decade")
-    .style("text-align", "center");
+    .text("Commuting to Work (percent)")
+      .style("text-align", "left")
+      .style("font-size", "18px");
 
   // column names
   thead
@@ -21,9 +22,7 @@ d3.csv("https://raw.githubusercontent.com/cheje/Interactive-Data-Vis-Sp2020/mast
     .data(data.columns)
     .join("th")
     .text(d => d)
-    //.style("border-right", "solid 1px #E8E8E8")
       .style("color","#808080");
-      //.style("font-style", "italic");
 
   thead
     .selectAll("th:first-child")
@@ -34,7 +33,7 @@ d3.csv("https://raw.githubusercontent.com/cheje/Interactive-Data-Vis-Sp2020/mast
     .append("tbody")
     .selectAll("tr")
     .data(data)
-    .join("tr")
+    .join("tr");
 
   // cells
   rows
@@ -43,12 +42,53 @@ d3.csv("https://raw.githubusercontent.com/cheje/Interactive-Data-Vis-Sp2020/mast
     .join("td")
     .text(d => d);
     
+  let minDrove = d3.min(data, function(d) { return d['Drove alone']; });
+  let maxDrove = d3.max(data, function(d) { return d['Drove alone']; });
+  let minCarpool = d3.min(data, function(d) { return +d.Carpooled; });
+  let maxCarpool = d3.max(data, function(d) { return +d.Carpooled; });
+
   rows
     .selectAll("td:not(:first-child)")
-      .style("color", d => (d <= 32) ? "#3182bd" : "#de2d26") // functions: https://www.w3schools.com/js/js_if_else.asp
-      .style("background-color", "#F8F8F8");
+      //.style("color", d => (d <= 10) ? "#808080" : "#de2d26"); // functions: https://www.w3schools.com/js/js_if_else.asp
+      // if above average, highlight in red: http://learnjsdata.com/summarize_data.html
+      // .style("color", function(d) {
+      //   let avgDrove = d3.mean(data, function(d) { return d['Drove alone']; });
+      //   if (d > avgDrove) {
+      //     return "#de2d26";
+      //   }
+      //   console.log(avgDrove);
+      .style("color", function(d) {
+        if (d[Array('Drove alone')] === minDrove || d['Drove alone'] === maxDrove) { return "#de2d26";}
+      console.log(maxCarpool);
+    });
+  
+    console.log(data.columns[1]);
 
   rows
     .selectAll("td:last-child")
       .style("border-right", "none");
+
+// averages row: https://stackoverflow.com/questions/37044713/how-can-we-sum-the-data-of-csv-file-columnwise-using-d3-js
+
+  let totalAvgs = [d3.mean(data.map(function(d){ return d['Drove alone'] })),
+    d3.mean(data.map(function(d){ return d.Carpooled })),
+    d3.mean(data.map(function(d){ return d['Public transportation'] })),
+    d3.mean(data.map(function(d){ return d.Walked })),
+    d3.mean(data.map(function(d){ return d['Taxi, motorcycle, bike, etc.'] })),
+    d3.mean(data.map(function(d){ return d['Worked at home'] }))];
+
+    console.log(totalAvgs);
+
+  totalAvgsRounded = d3.format(".1f")(4.56)
+        table
+        .append("tfoot")
+        .append("tr")
+        .text("Average")
+          .style("text-align", "right")
+          .style("font-weight", "bold")
+        .selectAll("td")
+        .data(totalAvgs)
+        .join("td")
+        .text(d => d);
 });
+
