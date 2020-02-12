e4fb1780a9baae74ac634e0c90569f9c2f172247
cheje <jcheng1@gradcenter.cuny.edu>
2020-02-12 12:48:32 -0500

    Add initial draft of bar chart

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -1,64 +1,78 @@
-// data load
-// reference for d3.autotype: https://github.com/d3/d3-dsv#autoType
-d3.csv("../../data/squirrelActivities.csv", d3.autoType).then(data => {
-  console.log(data);
+const data = [{"state":"Texas","amount":366},
+              {"state":"Illinois","amount":308},
+              {"state":"New York","amount":268},
+              {"state":"California","amount":199},
+              {"state":"Michigan","amount":103},
+              {"state":"Ohio","amount":83},
+              {"state":"Pennsylvania","amount":83},
+              {"state":"Florida","amount":73},
+              {"state":"Massachusetts","amount":67},
+              {"state":"North Carolina","amount":64},
+              {"state":"Louisiana","amount":59},
+              {"state":"Wisconsin","amount":57},
+              {"state":"Virginia","amount":52},
+              {"state":"Washington","amount":50},
+              {"state":"Missouri","amount":47}]
 
-  /** CONSTANTS */
-  // constants help us reference the same values throughout our code
-  const width = window.innerWidth * 0.9,
-    height = window.innerHeight / 3,
-    paddingInner = 0.2,
-    margin = { top: 20, bottom: 40, left: 40, right: 40 };
+const width = window.innerWidth * 0.5, // width of svg = half of browser window
+  height = window.innerHeight / 1.75,
+  paddingInner = 0.2, // padding in between bars
+  margin = { top: 20, bottom: 100, left: 10, right: 40 };
 
-  /** SCALES */
-  // reference for d3.scales: https://github.com/d3/d3-scale
-  const xScale = d3
-    .scaleBand()
-    .domain(data.map(d => d.activity))
-    .range([margin.left, width - margin.right])
-    .paddingInner(paddingInner);
+// scales transform/map data to lengths for bar charts
 
-  const yScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, d => d.count)])
-    .range([height - margin.bottom, margin.top]);
+const xScale = d3
+  .scaleBand() // divides up range between domain values: https://observablehq.com/@d3/d3-scaleband
+  .domain(data.map(d => d.state)) // state names
+  .range([margin.left, width - margin.right]) // 10, half of browser width - right margin of svg
+  .paddingInner(paddingInner);
 
-  // reference for d3.axis: https://github.com/d3/d3-axis
-  const xAxis = d3.axisBottom(xScale).ticks(data.length);
+const yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(data, d => d.amount)]) // 0, 366
+  .range([height - margin.bottom, margin.top]); // height of svg - 40, 20
 
-  /** MAIN CODE */
-  const svg = d3
-    .select("#d3-container")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+// reference for d3.axis: https://github.com/d3/d3-axis
+const xAxis = d3.axisBottom(xScale).ticks(data.length);
 
-  // append rects
-  const rect = svg
-    .selectAll("rect")
-    .data(data)
-    .join("rect")
-    .attr("y", d => yScale(d.count))
-    .attr("x", d => xScale(d.activity))
-    .attr("width", xScale.bandwidth())
-    .attr("height", d => height - margin.bottom - yScale(d.count))
-    .attr("fill", "steelblue")
+/** MAIN CODE */
+const svg = d3
+  .select("#d3-container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-  // append text
-  const text = svg
-    .selectAll("text")
-    .data(data)
-    .join("text")
-    .attr("class", "label")
-    // this allows us to position the text in the center of the bar
-    .attr("x", d => xScale(d.activity) + (xScale.bandwidth() / 2))
-    .attr("y", d => yScale(d.count))
-    .text(d => d.count)
-    .attr("dy", "1.25em");
+// append rects
+const rect = svg
+  .selectAll("rect")
+  .data(data)
+  .join("rect")
+  .attr("y", d => yScale(d.amount))
+  .attr("x", d => xScale(d.state))
+  .attr("width", xScale.bandwidth())
+  .attr("height", d => height - margin.bottom - yScale(d.amount))
+  .attr("fill", "steelblue")
 
-  svg
-    .append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(0, ${height - margin.bottom})`)
-    .call(xAxis);
-});
+// append text
+const text = svg
+  .selectAll("text")
+  .data(data)
+  .join("text")
+  .attr("class", "label")
+  // this allows us to position the text in the center of the bar
+  .attr("x", d => xScale(d.state) + (xScale.bandwidth() / 5))
+  .attr("y", d => yScale(d.amount))
+  .text(d => d.amount)
+  .attr("dy", "1.25em");
+
+svg
+  .append("g")
+  .attr("class", "axis")
+  .attr("transform", `translate(0, ${(height - margin.bottom)})`)
+  .call(xAxis)
+  .selectAll("text")
+  .style("text-anchor", "end")
+  .style("font-family", "Montserrat")
+  .attr("dx", "-0.7em")
+  .attr("dy", "0.1em")
+  .attr("transform", "rotate(-65)");
