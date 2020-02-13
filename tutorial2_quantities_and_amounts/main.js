f9af6e84a5312880c5651a16c87825645ad7bf69
cheje <jcheng1@gradcenter.cuny.edu>
2020-02-13 08:25:14 -0500

    Fix bars

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -14,8 +14,8 @@ const data = [{"state":"Texas","amount":366},
               {"state":"Washington","amount":50},
               {"state":"Missouri","amount":47}]
 
-const width = window.innerWidth * 0.33, // width of svg = half of browser window
-  height = window.innerHeight / 1.75,
+const width = window.innerWidth * 0.45, // width of svg = half of browser window
+  height = window.innerHeight / 1.5,
   paddingInner = 0.2, // padding in between bars
   margin = { top: 30, bottom: 100, left: 10, right: 40 };
 
@@ -92,13 +92,13 @@ svg
 // HORIZONTAL BAR CHART
 
 const hwidth = window.innerWidth * 0.5,
-  hheight = window.innerHeight / 1.5,
+  hheight = window.innerHeight / 1.25,
   hpaddingInner = 0.2,
-  hmargin = { top: 30, bottom: 100, left: 105, right: 300 };
+  hmargin = { top: 30, bottom: 100, left: 105, right: 40 };
 
 const hxScale = d3
   .scaleLinear()
-  .domain([0, d3.max(data, d => d.amount)])
+  .domain([d3.min(data, d => d.amount), d3.max(data, d => d.amount)])
   .range([hmargin.left, hwidth - hmargin.right]);
 
 const hyScale = d3
@@ -127,9 +127,14 @@ const hrect = hsvg
   .selectAll("rect")
   .data(data)
   .join("rect")
-    .attr("x", d => 58 + (d3.min(data, d => d.amount))) // https://stackoverflow.com/questions/25047562/d3-js-horizontal-bar-graph-change-bar-direction-left-to-right-instead-of-righ
+    // .attr("x", d => d3.min(data, d => d.amount)) // https://stackoverflow.com/questions/25047562/d3-js-horizontal-bar-graph-change-bar-direction-left-to-right-instead-of-righ
+    // .attr("y", d => hyScale(d.state))
+    // .attr("width", d => hxScale(d.amount))
+    // .attr("height", hyScale.bandwidth())
+    // .attr("fill", d => hbarColor(d.amount));
+    .attr("x", d => hxScale(d3.min(data, d => d.amount))) // https://stackoverflow.com/questions/25047562/d3-js-horizontal-bar-graph-change-bar-direction-left-to-right-instead-of-righ
     .attr("y", d => hyScale(d.state))
-    .attr("width", d => hxScale(d.amount) + 50)
+    .attr("width", d => d.amount + margin.left)
     .attr("height", hyScale.bandwidth())
     .attr("fill", d => hbarColor(d.amount));
 
@@ -140,11 +145,11 @@ const htext = hsvg
   .join("text")
   //.attr("class", "label")
     .attr("fill", d => hbarColor(d.amount))
-    .attr("x", d => hxScale(d.amount) + 150)
-    .attr("y", d => hyScale(d.state) + (hyScale.bandwidth() / 2.25))
+    .attr("x", d => d.amount + 150)
+    .attr("y", d => hyScale(d.state) + (hyScale.bandwidth() / 2))
   .text(d => d.amount)
-    .attr("text-anchor", "middle")
-    .attr("dx", "1.5em")
+  .attr("text-anchor", "end")
+    //.attr("dx", "1.5em")
     .attr("dy", "0.5em");
 
 // y-axis
